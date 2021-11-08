e3f9a210f7a022c32116384a2abd065a63fc5f0e
Abhiroop Reddy <abhiroopc84@gmail.com>
2021-11-08 12:52:21 +0530

    Update ssos_vince.mk

--- ssos_vince.mk
+++ ssos_vince.mk
@@ -28,7 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
 # Inherit some common ShapeShiftOS stuff.
-$(call inherit-product, vendor/ssos/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -39,13 +39,9 @@ TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
-# Inherit ShapeShiftOS common properties.
-PRODUCT_PRODUCT_PROPERTIES += \
-	ro.ssos.cpu=SD625
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := ssos_vince
+PRODUCT_NAME := aosp_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
